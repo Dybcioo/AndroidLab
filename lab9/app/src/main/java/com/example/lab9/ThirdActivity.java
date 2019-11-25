704acac01bd3846080415be81879c6da2a205711
Dominik <dybowski.dominik@o2.pl>
2019-11-25 16:43:48 +0100

    add task 3 [lab9]

---
+++ lab9/app/src/main/java/com/example/lab9/ThirdActivity.java
@@ -0,0 +1,17 @@
+package com.example.lab9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
